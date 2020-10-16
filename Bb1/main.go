dc287926d56a85f87eda035fb242bab362d1a725
gogirl110 <2976010448@qq.com>
2020-10-16 14:02:02 +0800

    learn cost

---
+++ Bb1/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	const Pi float64 = 3.14
+	const zero = 0.0
+
+	const (
+		size int64 = 1024
+		eof        = -1
+	)
+	const u, v float32 = 0, 3
+	const a, b, c = 3, 4, "foo"
+}
