ea98cf8c361348f3fa42cc0cb1ae01a0ea69061f
gogirl110 <2976010448@qq.com>
2020-10-20 21:46:02 +0800

    空(nil)切片

---
+++ Ee6/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers []int
+
+	printSlice(numbers)
+
+	if numbers == nil {
+		fmt.Printf("切片是空的")
+	}
+}
+
+func printSlice(x []int) {
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
+}
+
+//一个切片在未初始化之前默认为 nil，长度为 0
