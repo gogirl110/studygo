3e6e0fc1a825dcdd8f127d495e8ceae07345b8c0
gogirl110 <2976010448@qq.com>
2020-10-20 21:09:03 +0800

    结构体作为函数参数

---
+++ Ee3/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type Books struct {
+	title   string
+	author  string
+	subject string
+	book_id int
+}
+
+func main() {
+	var Book1 Books //声明Book1为Books类型
+	var Book2 Books //声明 Book2为Books类型
+
+	//book1描述
+	Book1.title = "Go 语言"
+	Book1.author = "www.runoob.com"
+	Book1.subject = "Go 语言教程"
+	Book1.book_id = 6495407
+
+	//book2描述
+	Book2.title = "Python 教程"
+	Book2.author = "www.runoob.com"
+	Book2.subject = "Python 语言教程"
+	Book2.book_id = 6495700
+
+	//打印Book1信息
+	printBook(Book1)
+
+	//打印Book2信息
+	printBook(Book2)
+}
+
+func printBook(book Books) {
+	fmt.Printf("Book title : %s\n", book.title)
+	fmt.Printf("Book author : %s\n", book.author)
+	fmt.Printf("Book subject : %s\n", book.subject)
+	fmt.Printf("Book book_id : %d\n", book.book_id)
+}
