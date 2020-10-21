fb423719ad86ab32d01cdeb8be00f2c7e4431d4c
gogirl110 <2976010448@qq.com>
2020-10-21 21:48:22 +0800

    delete()函数

---
+++ Ee11/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//创建map
+	countryCapitalMap := map[string]string{"France": "Paris", "Italy": "Rome", "Japan": "Tokyo", "India": "New delhi"}
+
+	fmt.Println("原始地图")
+
+	//打印地图
+	for country := range countryCapitalMap {
+		fmt.Println(country, "首都是", countryCapitalMap[country])
+	}
+
+	//删除元素
+	delete(countryCapitalMap, "France")
+	fmt.Println("法国条目被删除")
+
+	fmt.Println("删除元素后地图")
+
+	//打印地图
+	for country := range countryCapitalMap {
+		fmt.Println(country, "首都是", countryCapitalMap[country])
+	}
+}
+
+//delete() 函数用于删除集合的元素, 参数为 map 和其对应的 key
