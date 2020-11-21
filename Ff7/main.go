334ea7dcce30d9ac53f7216862f5d862972e5771
gogirl110 <2976010448@qq.com>
2020-11-21 18:03:57 +0800

    Print Printf Println的区别

--- Ff7/main.go
+++ Ff7/main.go
@@ -1,20 +1,20 @@
-package main 
+package main
 
 import "fmt"
 
 func main() {
-	//fmt.Println("你好，golang")
+	fmt.Println("你好，golang")
 	//fmt.Print("你好，golang")
 	//fmt.Printf("你好，golang")
 
 	/*
-	Print和Println区别
-	Println会自动换行，Print不会
-	一次输出多个值时，Println中间有空格，Print没有
+		Print和Println区别
+		Println会自动换行，Print不会
+		一次输出多个值时，Println中间有空格，Print没有
 	*/
 
 	/*
-	Println和Printf区别
-	Printf是格式化输出，在很多场景下比Println更方便
+		Println和Printf区别
+		Printf是格式化输出，在很多场景下比Println更方便
 	*/
 }
