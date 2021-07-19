3036a29f1a4b6f7aa94f8e5aee9b952eeef0ec06
gogirl110 <2976010448@qq.com>
2021-07-19 12:59:11 +0800

    学习

---
+++ Bb4/main.go
@@ -0,0 +1,37 @@
+package main
+
+// 学习 chan 无缓冲
+// 这里用到 go  for chan  select基本技术
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan int)
+	go func() {
+		i := 0
+		for {
+			time.Sleep(1 * time.Second)
+			c1 <- i
+			if i == 10 {
+				close(c1)
+				break
+			}
+			i++
+		}
+	}()
+
+	for {
+		select {
+		case n1, ok := <-c1:
+			if !ok {
+				return
+			}
+			fmt.Println("recvie", n1)
+		default:
+			break
+		}
+	}
+}
