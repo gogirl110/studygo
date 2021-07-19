be89c13235882e5de32856c9b84592570da4a20a
gogirl110 <2976010448@qq.com>
2021-07-19 13:27:06 +0800

    概述函数

--- learn02/main.go
+++ learn02/main.go
@@ -1,35 +1,50 @@
 package main
 
-// 学习 chan 无缓冲
-// 这里用到 go  for chan  select基本技术
+/*
+//函数可以定义多个返回值，甚至对其命名
+import (
+	"errors"
+	"fmt"
+)
 
-import "time"
-import "fmt"
+func div(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero")
+	}
+	return a / b, nil
+}
 
 func main() {
-	c1 := make(chan int)
-	go func() {
-		i := 0 
-		for {
-			time.Sleep(1 * time.Second) 
-			c1 <- i
-			if i == 10 {
-				close(c1)
-				break
-			}
-			i ++
-		}
-	}()
+	a, b := 10, 2
+	c, err := div(a, b)
 
-	for {
-		select {
-		case n1, ok:= <-c1:
-			if !ok{
-				return 
-			}
-			fmt.Println("recvie", n1)
-		 default:
-			break
-		}
+	fmt.Println(c, err)
+}
+*/
+
+/*
+//函数是第一类型，可作为参数或返回值
+func test(x int) func() {
+	return func() {
+		println(x)
 	}
+}
+
+func main() {
+	x := 100
+
+	f := test(x)
+	f()
+}
+*/
+
+//用defer定义延迟调用，无论函数是否出错，它都确保结束前被调用
+func test(a, b int) {
+	defer println("dispose...")
+
+	println(a / b)
+}
+
+func main() {
+	test(10, 0)
 }
