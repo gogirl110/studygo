9c2bc6c1e53e14639d6e1d9eda81feef294980d9
gogirl110 <2976010448@qq.com>
2021-07-20 22:50:20 +0800

    基本类型

---
+++ learn08/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+/*
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	a, b, c := 100, 0144, 0x64
+
+	fmt.Println(a, b, c)
+	fmt.Printf("0b%b,%#o,%#x\n", a, a, a)
+
+	fmt.Println(math.MinInt8, math.MaxInt8)
+}
+//支持八进制、十六进制以及科学计数法。标准库math定义了各数字类型的取值范围
+*/
+
+/*
+//标准库strconv可在不同进制（字符串）间转换
+import "strconv"
+
+func main() {
+	a, _ := strconv.ParseInt("1100100", 2, 32)
+	b, _ := strconv.ParseInt("0144", 8, 32)
+	c, _ := strconv.ParseInt("64", 16, 32)
+
+	println(a, b, c)
+
+	println("0b" + strconv.FormatInt(a, 2))
+	println("0" + strconv.FormatInt(a, 8))
+	println("0x" + strconv.FormatInt(a, 16))
+}
+*/
+
+//使用浮点数时，须注意小数位的有效精度
+func main() {
+	var a float32 = 1.1234567899 //注意：默认浮点类型是float64
+	var b float32 = 1.12345678
+	var c float32 = 1.123456781
+
+	println(a, b, c)
+	println(a == b, a == c)
+	fmt.Printf("%v %v, %v\n", a, b, c)
+}
